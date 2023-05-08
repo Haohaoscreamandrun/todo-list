136f40d32a2049f382cc198e0e5213ce5a1106ec
Haohaoscreamandrun <j842671395@gmail.com>
2023-05-08 20:46:13 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 // use dotenv only not in production env
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
@@ -14,6 +15,9 @@ const Todo = require('./models/todo')
 // set template engine
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'})) //建立一個名為hbs的樣板引擎，並傳入exphbs為相關參數
 app.set('view engine','hbs') //啟用樣板引擎hbs
+
+// decrypt request body
+app.use(bodyParser.urlencoded({extended: true}))
 
 //Connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -29,13 +33,29 @@ db.once('open',()=>{
 
 // Setting route
 app.get('/',(req,res)=>{
+  // Controller
   Todo
     .find() //取出Todo model裡所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index',{ todos })) // 將資料傳給 index 樣板
+    .then(todos => res.render('index',{ todos })) // view: 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
 
+  // Add new todo
+app.get('/todos/new',(req,res)=>{
+  res.render('new')
+})
+
+app.post('/todos',(req,res)=>{  //往/todos post new todo
+  const name = req.body.name //從req.body拿出表單裡name的資料
+  // const todo = new Todo({ name: name }) //在伺服器端建立Todo實例
+  // return todo
+  //   .save()   // 存入資料庫中
+  return Todo.create({name}) //命令mongoose，用該資料建立一個todo
+    .then(()=> res.redirect('/')) //把使用者送回首頁
+    .catch(error => console.log(error)) //錯誤處理
+  
+})
 
 // Listening to port 3000
 app.listen(port,()=>{
--- models/todo.js
+++ models/todo.js
@@ -1,3 +1,4 @@
+// Model
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const todoSchema = new Schema({
