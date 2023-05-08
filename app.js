91c3dcb0e89d2cde9eba4a4d96731abe30f06d38
Haohaoscreamandrun <j842671395@gmail.com>
2023-05-08 19:45:16 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // Require packages
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 // use dotenv only not in production env
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
 }
 const app = express()
 const port = 3000
+
+// set template engine
+app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'})) //建立一個名為hbs的樣板引擎，並傳入exphbs為相關參數
+app.set('view engine','hbs') //啟用樣板引擎hbs
+
 //Connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -21,8 +27,9 @@ db.once('open',()=>{
 
 // Setting route
 app.get('/',(req,res)=>{
-  res.send('Hello World')
+  res.render('index')
 })
+
 
 // Listening to port 3000
 app.listen(port,()=>{
