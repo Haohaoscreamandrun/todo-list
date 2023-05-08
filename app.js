bd23ac14fa7b8653ac5eb1ab2a7d0cd80e3aecf5
Haohaoscreamandrun <j842671395@gmail.com>
2023-05-08 20:02:49 +0800

    feat: modify index.js

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ if(process.env.NODE_ENV !== 'production'){
 const app = express()
 const port = 3000
 
+const Todo = require('./models/todo')
+
 // set template engine
 app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'})) //建立一個名為hbs的樣板引擎，並傳入exphbs為相關參數
 app.set('view engine','hbs') //啟用樣板引擎hbs
@@ -27,7 +29,11 @@ db.once('open',()=>{
 
 // Setting route
 app.get('/',(req,res)=>{
-  res.render('index')
+  Todo
+    .find() //取出Todo model裡所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index',{ todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 
