94207e7f7beef1ac9614d947690b716b28fa03f5
Haohaoscreamandrun <j842671395@gmail.com>
2023-05-13 22:23:27 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ app.get('/',(req,res)=>{
   Todo
     .find() //取出Todo model裡所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    // .sort({name: "asc"}) //按照名字的順序排列，反敘desc
+    .sort({_id: "asc"}) //按照id順序(創立時間)
     .then(todos => res.render('index',{ todos })) // view: 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
@@ -83,7 +85,7 @@ app.post('/todos/:id/edit',(req,res)=>{
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
-      todo.isDone = (isDone === 'on')
+      todo.isDone = (isDone === 'on') // on or nothing
       return todo.save()
       //這次因為搭配的資料操作是 Todo.findById，這個方法只會返回一筆資料，所以後面需要接 todo.save() 針對這一筆資料進行儲存，而非操作整份資料。
     })
